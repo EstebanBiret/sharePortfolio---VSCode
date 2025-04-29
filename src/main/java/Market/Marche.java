49c47711e25713ea40ae8d88ca131284827d4443
unknown <max.segot@gmail.com>
2025-04-29 15:30:11 +0200

    Creation des classes Investisseurs, Gestionnaire et Marche. Segot Maxime pilote, Junfang He copilot

--- src/main/java/Market/Marche.java
+++ src/main/java/Market/Marche.java
@@ -16,14 +16,27 @@ public class Marche {
      */
     private static HashMap<Action, Integer> actionsAvailable;
 
+
+    /**
+     * Constructor of the Marche class
+     */
     public Marche() {
         this.actionsAvailable = new HashMap<>();
     }
 
+    /**
+     * Constructor of the Marche class
+     * @param actionsAvailable the actions available on the market
+     */
     public Marche(HashMap<Action, Integer> actionsAvailable) {
         this.actionsAvailable = actionsAvailable;
     }
 
+    /**
+     * Allows to add an action to the market
+     * @param action the action to add
+     * @param quantity the quantity of actions to add
+     */
     public HashMap<Action, Integer> getActionsAvailable() {
         return actionsAvailable;
     }
--- src/main/java/fr/utc/miage/Acteurs/Gestionnaire.java
+++ src/main/java/fr/utc/miage/Acteurs/Gestionnaire.java
@@ -1,5 +1,59 @@
+/*
+ * Copyright 2025 Segot-Laberou Maxime, Junfang He;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fr.utc.miage.Acteurs;
 
-public class Gestionnaire {
+import fr.utc.miage.shares.Action;
+
+/**
+ * This class represents a manager.
+ * 
+ * @author Maxime Segot-Laberou, Junfang He
+ */
+public class Gestionnaire extends Personne {
+
+    /**
+     * Constructor of the Gestionnaire class
+     * @param nom the name of the manager
+     * @param prenom the first name of the manager
+     * @param password the password of the manager
+     */
+    public Gestionnaire(String nom, String prenom, String password) {
+        super(nom, prenom, password);
+    }
+
+    /**
+     * Allows to create an action
+     * @param libelle the name of the action
+     * @return true if the action was created, false otherwise
+     */
+    public boolean createAction(Action action) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean deleteAction(Action action) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean updateAction(Action action) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
 
 }
